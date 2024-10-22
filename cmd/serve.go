391ed09a3807667e7b8dd5e820761b49a5a2cc16
Adam Pickering <adam.pickering@suse.com>
2024-10-21 18:14:52 -0600

    Add file watching to serve subcommand

--- cmd/serve.go
+++ cmd/serve.go
@@ -1,9 +1,16 @@
 package cmd
 
 import (
+	"context"
+	"errors"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"sync"
 
+	"github.com/fsnotify/fsnotify"
 	"github.com/spf13/cobra"
 )
 
@@ -13,16 +20,89 @@ func init() {
 
 var serveCmd = &cobra.Command{
 	Use:   "serve <addr>",
-	Short: "A brief description of your command",
+	Short: "Serve static site",
 	Args:  cobra.ExactArgs(1),
 	RunE:  runServe,
 }
 
-func runServe(cmd *cobra.Command, args []string) error {
+var buildMutex = sync.Mutex{}
+
+func exclusiveBuild(op string, filePath string) error {
+	if !buildMutex.TryLock() {
+		return nil
+	}
+	defer buildMutex.Unlock()
+	log.Printf("build triggered by %s on %s", op, filePath)
 	if err := build(); err != nil {
 		return fmt.Errorf("failed to build: %w", err)
 	}
-	handler := http.FileServer(http.Dir(outputPath))
-	fmt.Printf("Listening on %s\n", args[0])
-	return http.ListenAndServe(args[0], handler)
+	return nil
+}
+
+func runServe(cmd *cobra.Command, args []string) error {
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
+
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		return fmt.Errorf("failed to construct watcher: %w", err)
+	}
+	defer watcher.Close()
+
+	for _, dirPath := range []string{templatesPath, contentPath, staticPath} {
+		if err := watcher.Add(dirPath); err != nil {
+			return fmt.Errorf("failed to watch %s: %w", dirPath, err)
+		}
+	}
+
+	go func() {
+		op := "INITIAL_BUILD"
+		filePath := "N/A"
+	forloop:
+		for {
+			if err := exclusiveBuild(op, filePath); err != nil {
+				log.Println(err)
+			}
+
+			select {
+			case _, ok := <-ctx.Done():
+				if !ok {
+					break forloop
+				}
+			case event, ok := <-watcher.Events:
+				if !ok {
+					break forloop
+				}
+				op = event.Op.String()
+				filePath = event.Name
+			case err, ok := <-watcher.Errors:
+				if !ok {
+					break forloop
+				}
+				log.Printf("error from watcher: %s", err)
+				break forloop
+			}
+		}
+		stop()
+	}()
+
+	server := http.Server{
+		Addr:    args[0],
+		Handler: http.FileServer(http.Dir(outputPath)),
+	}
+
+	go func() {
+		log.Printf("listening on %s", args[0])
+		if err := server.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			log.Printf("http server failed: %s", err)
+			stop()
+		}
+	}()
+
+	<-ctx.Done()
+
+	if err := server.Shutdown(context.Background()); err != nil {
+		return fmt.Errorf("error while shutting down server: %w", err)
+	}
+
+	return nil
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/adamkpickering/adamkpickering.github.io
 go 1.23.2
 
 require (
+	github.com/fsnotify/fsnotify v1.7.0
 	github.com/spf13/cobra v1.8.1
 	github.com/yuin/goldmark v1.7.8
 	gopkg.in/yaml.v3 v3.0.1
@@ -11,4 +12,5 @@ require (
 require (
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	golang.org/x/sys v0.4.0 // indirect
 )
